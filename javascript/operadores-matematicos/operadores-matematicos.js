e4c672237a4b3b280d29450fe84c8cf7b54450fe
Ian Fonseca Araújo <150860782+ianfonseca@users.noreply.github.com>
2026-08-25 20:33:55 -0300

    Operadores Matemáticos

---
+++ javascript/operadores-matematicos/operadores-matematicos.js
@@ -0,0 +1,42 @@
+//soma de dois numeros
+let num1 = 15;
+let num2 = 10;
+let soma = num1 + num2;
+console.log("A soma de " + num1 + "+" + num2 + " é igual a: " + soma);
+
+//soma de tres numeros
+let num3 = 5;
+let somaTres = num1 + num2 + num3;
+console.log("A soma de " + num1 + "+" + num2 + "+" + num3 + " é igual a: " + somaTres);
+
+//subtração de dois numeros
+let subtracao = num1 - num2;
+console.log("A subtração de " + num1 + "-" + num2 + " é igual a: " + subtracao);
+
+//subtração de tres numeros
+let subtracaoTres = num1 - num2 - num3;
+console.log("A subtração de " + num1 + "-" + num2 + "-" + num3 + " é igual a: " + subtracaoTres);
+
+//multiplicação de dois numeros
+let multiplicacao = num1 * num2;
+console.log("A multiplicação de " + num1 + "x" + num2 + " é igual a: " + multiplicacao);
+
+//multiplicação de tres numeros
+let multiplicacaoTres = num1 * num2 * num3;
+console.log("A multiplicação de " + num1 + "x" + num2 + "x" + num3 + " é igual a: " + multiplicacaoTres);
+
+//divisão de dois numeros
+let divisao = num1 / num2;
+console.log("A divisão de " + num1 + "/" + num2 + " é igual a: " + divisao);
+
+//divisão de tres numeros
+let divisaoTres = num1 / num2 / num3;
+console.log("A divisão de " + num1 + "/" + num2 + "/" + num3 + " é igual a: " + divisaoTres);
+
+//Resto da divisão de dois numeros
+let restoDivisao = num1 % num2;
+console.log("O resto da divisão de " + num1 + "%" + num2 + " é igual a: " + restoDivisao);
+
+//Resto da divisão de tres numeros
+let restoDivisaoTres = num1 % num2 % num3;
+console.log("O resto da divisão de " + num1 + "%" + num2 + "%" + num3 + " é igual a: " + restoDivisaoTres);
