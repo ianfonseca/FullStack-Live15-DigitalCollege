6ee63f8ad2c396fdf843724c6f1a5c33061bb7bb
Ian Fonseca Araújo <150860782+ianfonseca@users.noreply.github.com>
2026-08-25 21:09:07 -0300

    Laços de Repetição

---
+++ javascript/lacos-repeticao/lacos-repeticao.js
@@ -0,0 +1,18 @@
+//Laço "for" - Estrutura de repetição com teste lógico no início
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+//Laço "while" - Estrutura de repetição com teste lógico no início
+let j = 1;
+while (j <= 18) {
+    console.log("Você tem " + j + " anos.");
+    j++;
+}
+
+//Laço "do...while" - Estrutura de repetição com teste lógico no final
+let k = 7;
+do {
+    console.log(k);
+    k++;
+} while (k <= 10);
