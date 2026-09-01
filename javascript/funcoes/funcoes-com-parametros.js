742173851e0aa354dc3af2032ec30ef4f6e00fe8
Ian Fonseca Araújo <150860782+ianfonseca@users.noreply.github.com>
2026-09-01 16:53:39 -0300

    Estudos sobre funcoes

---
+++ javascript/funcoes/funcoes-com-parametros.js
@@ -0,0 +1,19 @@
+console.warn("ESTUDO DE FUNÇÕES COM PARÂMETROS")
+
+//Função com 1 parâmetro
+function verNome(nome){
+    console.log(`Meu nome é ${nome}`);
+}
+
+verNome("Ian")
+verNome("Lívia ")
+verNome("Liz")
+
+//Função com 2 parâmetros
+function soma(n1, n2){
+    console.log(`A soma de ${n1} e ${n2} é igual a ${n1 + n2}`)
+}
+
+soma(30,40)
+soma(70,20)
+soma(80,90)
---
+++ javascript/funcoes/funcoes-sem-parametros.js
@@ -0,0 +1,7 @@
+// Funçõs padrão sem parametros
+console.warn("ESTUDO DE FUNÇÕES SEM PARÂMETROS")
+function exibeNome(){
+    console.log("Meu nome é Ian");
+}
+
+exibeNome()
