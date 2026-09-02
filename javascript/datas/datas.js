74de85fdeb49dcdd40749a7c7b066dbd26235bf0
Ian Fonseca Araújo <150860782+ianfonseca@users.noreply.github.com>
2026-09-02 15:45:11 -0300

    Trabalhando com datas

---
+++ javascript/datas/datas.js
@@ -0,0 +1,9 @@
+// Criando uma data
+const dataAtual = new Date()
+console.log("dataAtual.getDate() retorna o dia do mês. Exe.:", dataAtual.getDate())
+console.log("dataAtual.getDay() retorna o dia da semana. Exe.:", dataAtual.getDay())
+console.log("dataAtual.getMonth() retorna mês atual. Exe.:", dataAtual.getMonth())
+console.log("dataAtual.getHours() retorna a hora atual. Exe.:",dataAtual.getHours())
+console.log("dataAtual.getFullYear() retorna o ano atual. Exe.:", dataAtual.getFullYear())
+console.log(`${dataAtual.getHours()}h${dataAtual.getMinutes()}`)
+
