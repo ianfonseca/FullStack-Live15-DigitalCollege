d7e4256705d246d9f975588b0d6874dcff61906c
Ian Fonseca Araújo <150860782+ianfonseca@users.noreply.github.com>
2026-09-08 16:36:33 -0300

    Criar primeira parde de todo list

---
+++ javascript/projetos/to-do-list/script.js
@@ -0,0 +1,19 @@
+const btnAddTask = document.getElementById("btn-add-task")
+const tBody = document.getElementById("list-task")
+
+
+btnAddTask.addEventListener("click", function() {
+    const inputTask = document.getElementById("input-task")
+    const listTask = document.getElementById("task-list")
+    const tr = document.createElement("tr")
+    const taskTd = document.createElement("td")
+    const actionsTd = document.createElement("td")
+    actionsTd.innerHTML = "Editar - Concluir - Rejeitar"
+    taskTd.innerHTML = inputTask.value
+
+    tr.appendChild(taskTd)
+    tr.appendChild(actionsTd)
+    tBody.appendChild(tr)
+
+    console.log(tBody)
+})
